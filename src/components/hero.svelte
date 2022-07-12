548b0d4f2ce6347e31300d80304aa994983e49f0
unknown <agustindavid@gmail.com>
2022-07-11 22:17:55 -0500

    fonts

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -5,6 +5,6 @@
         <img src="reto-logo.png" class="z-10" alt="logo">
     </div>
     <div class=" mt-24 ">
-        <a class=" transition-all hover:bg-fuga-pink hover:text-white rounded-md py-2 px-8 text-2xl uppercase bg-white " href="https://chronosport.mx/carrera/22646/funcion/96907" target="_blank">Inscríbete aquí</a>
+        <a class=" transition-all hover:bg-fuga-pink hover:text-white rounded-md py-2 px-8 text-2xl uppercase font-black font-monserrat  bg-white " href="https://chronosport.mx/carrera/22646/funcion/96907" target="_blank">Inscríbete aquí</a>
     </div>
 </div>
